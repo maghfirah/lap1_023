25f6707b168151ecde13df09d11161a75f806971
Maghfirah Suyuti <60200112023@uin-alauddin.ac.id>
2015-04-22 17:03:18 +0800

    Update and rename sampel.java to revisi.java

---
+++ revisi.java
@@ -0,0 +1,19 @@
+package pbo1_023;
+
+public class tampil {
+	
+	 static String nama="Kenalkan, saya Maghfirah Suyuti";
+	 static int a = 2, b =2 ; 
+     
+	 public static void main(String[] args) {
+	        System.out.println("Praktikum Pertama");
+	        System.out.println(nama);
+	        System.out.println("Nilai a = "+a);
+	        System.out.println("Nilai b = "+b);
+	        System.out.print("a+b = "); System.out.println(a+b);
+	        System.out.print("a-b = "); System.out.println(a-b);
+	        System.out.print("a*b = "); System.out.println(a*b);
+	        System.out.print("a/b = "); System.out.println(a/b);  
+	        
+	    }
+}
