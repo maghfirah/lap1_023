1511d53767835ceee5f18f5123ff656571ed02ab
Maghfirah Suyuti <60200112023@uin-alauddin.ac.id>
2015-04-22 16:10:12 +0800

    Create tampil.java
    kak gun, ini hasil revisi praktikum 1..

---
+++ tampil.java
@@ -0,0 +1,18 @@
+package pbo1_023;
+
+public class tampil {
+	
+	 static String nama="Kenalkan, saya Maghfirah Suyuti";
+	 
+	 public static void main(String[] args) {
+	        int a = 2, b =2 ; 
+	        int c;
+	        
+	        c = a+b;
+	        
+	        System.out.println("Praktikum Pertama");
+	        System.out.println(nama);
+	        System.out.println("Penjumlahan nilai a="+a+" ditambah b="+b +" sama dengan" + c);
+	        
+	    }
+}
