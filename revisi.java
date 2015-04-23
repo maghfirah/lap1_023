016b534f67a60c6eae7fb62db9d9fe6e8a9c2999
Maghfirah Suyuti <60200112023@uin-alauddin.ac.id>
2015-04-23 16:22:12 +0800

    Update revisi.java

--- revisi.java
+++ revisi.java
@@ -4,6 +4,11 @@ public class tampil {
 	
 	 static String nama="Kenalkan, saya Maghfirah Suyuti";
 	 static int a = 2, b =2 ; 
+	 
+	 void identitas (){
+	 	System.out.println("Nama Panggilan: Virah");
+	 	System.out.println("Nama Panggilan: Virah");
+	 }
      
 	 public static void main(String[] args) {
 	        System.out.println("Praktikum Pertama");
